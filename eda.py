be7ebbb02c2f943b1aaacce0d23b1f9eb2e097d5
Tushar_Khitoliya <158598504+KHITOLIA@users.noreply.github.com>
2025-10-29 01:41:43 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -73,7 +73,7 @@ def dashboard():
     if page == "Overview":
        
        st.title("Dataset Overview")
-       st.write(df.head(4))
+       st.write(df.head(3))
        st.subheader("Statistical Summary")
        st.write(df.describe())
        st.write("### Categorical Value Counts")
